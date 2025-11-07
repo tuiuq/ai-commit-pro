b88776eae1ddd9c63c086927a28ae60cade4c354
tuiu <13719283454@163.com>
2025-11-07 18:03:32 +0800

    feat(utils): add PromptTemplate class for dynamic prompt generation

---
+++ src/utils/Template.ts
@@ -0,0 +1,32 @@
+export class PromptTemplate<P extends Record<string, string | string[] | undefined>> {
+  private readonly ifBlockRegex = /\{\{#if\s+([a-zA-Z0-9_]+)\s*\}\}([\s\S]*?)\{\{#endif\s*\}\}/g;
+  private readonly variableRegex = /\{\{\s*([a-zA-Z0-9_]+)\s*\}\}/g;
+
+  constructor(private prompt: string, private promptData: P) {
+    this.processTemplate();
+  }
+  
+  private processTemplate(): void {
+    this.prompt = this.prompt.replace(
+      this.ifBlockRegex,
+      (_, condition: keyof P, content: string) => {
+        return this.promptData[condition] ? content : "";
+      }
+    )
+    
+    this.prompt =this.prompt.replace(
+      this.variableRegex,
+      (_, key: keyof P) => {
+        const value = this.promptData[key];
+        if (Array.isArray(value)) {
+          return value.join("\n");
+        }
+        return value ?? ""
+      }
+    )
+  }
+  
+  public toString(): string {
+    return this.prompt.trim();
+  }
+}
