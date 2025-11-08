5551af14aa09251dee089ee3a3471f30abbde145
tuiu <13719283454@163.com>
2025-11-08 17:29:47 +0800

    feat(utils): 🎉 Add getTopLevel function to determine the top-level directory of the git repository
    Introduce a new function  to resolve and return the top-level directory path of the git repository, improving the functionality of git operations within the application. This change also updates the  function to use the resolved path for accurate file referencing.

--- src/utils/simpleGit.ts
+++ src/utils/simpleGit.ts
@@ -1,11 +1,16 @@
 import { execSync } from "node:child_process";
+import { resolve } from "node:path";
+
+export function getTopLevel() {
+  return execSync("git rev-parse --show-toplevel", { encoding: "utf-8" }).trim()
+}
 
 export function getChangedFiles(): string[] {
   return execSync("git diff --cached --name-only", { encoding: "utf-8" }).split("\n").filter(file => file.trim() !== "")
 }
 
 export function getGitDiffWithFile(file: string): string {
-  return execSync(`git diff --cached "${file}"`, { encoding: "utf-8" }).trim()
+  return execSync(`git diff --cached "${resolve(getTopLevel(), file)}"`, { encoding: "utf-8" }).trim()
 }
 
 export function getGitDiff(): string[] {
