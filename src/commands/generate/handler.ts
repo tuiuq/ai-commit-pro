17ba96ee5e80db28caebd17f520c5dfa46b60392
tuiu <13719283454@163.com>
2025-11-11 01:42:38 +0800

    refactor(generate): ♻️ 简化 commit 操作流程

--- src/commands/generate/handler.ts
+++ src/commands/generate/handler.ts
@@ -2,9 +2,7 @@ import {commit} from "@/utils/simpleGit.ts";
 
 export function handleOutput(message: string, shouldCommit: boolean) {
   if (shouldCommit) {
-    console.log(message)
-    const result = commit(message)
-    console.log(result)
+    commit(message)
     console.log("Changes committed successfully.")
   } else {
     console.error(message)
