c05d4511c48408e86113055dc7db148851ca243e
tuiu <13719283454@163.com>
2025-11-14 22:53:00 +0800

    feat(generate): 🎉 支持生成Commit Message的详细日志输出
    - 添加了对verbose选项的支持，允许用户查看生成过程中的详细信息。
    - 使用logger记录生成过程中的关键信息，包括选项和环境变量。

--- src/commands/generate/index.ts
+++ src/commands/generate/index.ts
@@ -2,17 +2,30 @@ import { Generator } from "@core/Generator.ts";
 import { EnvironmentSchema } from "@core/types.ts";
 import { Command } from "commander"
 import { GenerateOptions } from "./types.ts";
+import { logger } from "@/utils/Logger.ts";
 
 const generateCommand = new Command()
+
+const env = EnvironmentSchema.parse(process.env)
 
 generateCommand
   .name("generate")
   .alias("g")
   .description("使用 AI 生成Commit Message信息")
   .option("-l, --lang <language>", "指定生成的Commit Message语言", "zh")
-  .option("-v, --verbose", "显示详细信息")
-  .action((options: GenerateOptions) => {
-    const generator = new Generator(EnvironmentSchema.parse(process.env))
+  .option("-v, --verbose", "显示详细信息", false)
+  .action(async (options: GenerateOptions) => {
+    if (options.verbose) {
+      logger.setLevel("debug")
+    }
+
+    logger.debug("options", options)
+    logger.debug("env: ", {
+      model: env.OPENAI_MODEL,
+      baseURL: env.OPENAI_BASE_URL,
+    })
+    const generator = new Generator(env)
+    
   })
 
 export default generateCommand
